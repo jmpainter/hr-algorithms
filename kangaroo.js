e0300986ed1b0bf1dde85ba51eb94e5589d99ada
jmpainter <jpainter4@gmail.com>
2019-04-09 14:15:36 -0700

    added kangaroo

---
+++ kangaroo.js
@@ -0,0 +1,22 @@
+function kangaroo(x1, v1, x2, v2) {
+  debugger;
+  if ((x1 < x2 && v1 < v2) || (x2 < x1 && v2 < v1)) {
+    return 'NO';
+  }
+
+  if (v1 > v2) {
+    while (x1 < x2) {
+      x1 += v1;
+      x2 += v2;
+    }
+  } else {
+    while (x2 < x1) {
+      x1 += v1;
+      x2 += v2;
+    }
+  }
+
+  return x1 === x2 ? 'YES' : 'NO';
+}
+
+console.log(kangaroo(0, 3, 4, 2));
