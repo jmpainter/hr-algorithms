34fde04ad115e9e1d06e28f07ff06689b548ad34
jmpainter <jpainter4@gmail.com>
2019-04-09 13:57:12 -0700

    added grading-students

---
+++ grading-students.js
@@ -0,0 +1,12 @@
+function gradingStudents(grades) {
+  for (let i = 0; i < grades.length; i++) {
+    if (grades[i] >= 38) {
+      if (grades[i] % 5 >= 3) {
+        grades[i] += 5 - (grades[i] % 5);
+      }
+    }
+  }
+  return grades;
+}
+
+console.log(gradingStudents([73, 67, 38, 33]));
